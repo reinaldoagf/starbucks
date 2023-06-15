72a804cd7943340334e831d70195f548dfaebd85
unknown <reinaldoagf1@gmail.com>
2023-06-14 20:47:55 -0400

    fix: meadia queries updated

--- js/app.js
+++ js/app.js
@@ -23,7 +23,36 @@ $(document).ready(function() {
       fade: false,
       centerMode: true,
       variableWidth: true,
-      arrows: false
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: true,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     });
     $('#main__content__carousel').on('beforeChange', function(event, slick, currentSlide, nextSlide){
       $(".slick-arrow").removeClass("slick-arrow-animated");
